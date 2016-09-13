a05e873609c2e17d0bca89fca39f3b615fbf1dca
Matias Seijas <matias@tanookilabs.com>
2016-09-13 12:55:05 -0400

    Implement GiphyRequest model

--- GiphySwift/GiphySwift/GiphyRequest.swift
+++ GiphySwift/GiphySwift/GiphyRequest.swift
@@ -1,101 +1,17 @@
-//: Playground - noun: a place where people can play
+//
+//  Request.swift
+//  GiphySwift
+//
+//  Created by Matias Seijas on 9/13/16.
+//  Copyright © 2016 Matias Seijas. All rights reserved.
+//
 
-import UIKit
 import Foundation
-import PlaygroundSupport
-
-PlaygroundPage.current.needsIndefiniteExecution = true
-
-public struct Giphy {
-    
-    static let host = "api.giphy.com"
-    static let apiVersion = "v1"
-    static let baseUrl = "https://\(host)/\(apiVersion)"
-    static let publicApiKey = "dc6zaTOxFJmzC"
-    
-    
-    
-    enum Rating {
-        case y, g, pg, pg13, r
-    }
-    
-    enum ApiKey {
-        case `public`, `private`(key: String)
-        
-        var key: String {
-            switch self {
-            case .public: return publicApiKey
-            case .private(let key): return key
-            }
-        }
-    }
-    
-    private(set) static var apiKey = ApiKey.public.key
-    
-    func configure(with apiKey: ApiKey) {
-        Giphy.apiKey = apiKey.key
-    }
-    
-    static func request(_ request: Request.Gif) {
-        print(request)
-    }
-}
-
-//protocol GiphyEndpoint {
-//    func request()
-//}
-//
-//extension GiphyEndpoint {
-//    func request() {
-//        Giphy.request(self)
-//    }
-//}
-
-//protocol GifRequestable: GiphyEndpoint {
-//    //var type: GifRequest.Type { get }
-//    var limit: Int { get }
-//    var offset: Int { get }
-//    var rating: Giphy.Rating? { get }
-//}
-
-//
-//struct EndpointOptions {
-//    
-//    struct Gifs {
-//        let limit: Int
-//        let offset: Int
-//        let rating: Giphy.Rating?
-//        
-//        init(limit: Int = 25, offset: Int = 0, rating: Giphy.Rating? = nil) {
-//            self.limit = limit
-//            self.offset = offset
-//            self.rating = rating
-//        }
-//    }
-//    
-//    struct Stickers {
-//        
-//    }
-//    
-//}
-
-// MARK: - GiphyRequestable
-fileprivate protocol GiphyRequestable {
-    var urlComponents: String { get }
-    var url: URL { get }
-}
-
-extension GiphyRequestable {
-    var url: URL {
-        let url = Giphy.baseUrl + urlComponents + "&api_key=\(Giphy.apiKey)"
-        return URL(string: url)!
-    }
-}
 
 
-// MARK: - GiphyRequest
 struct GiphyRequest { }
 
+// MARK: - GiphyRequset: Gif
 extension GiphyRequest {
     enum Gif {
         
@@ -157,6 +73,7 @@ extension GiphyRequest {
 }
 
 
+// MARK: - GiphyRequset: Sticker
 extension GiphyRequest {
     enum Sticker {
         
@@ -210,5 +127,19 @@ extension GiphyRequest {
             }
         }
         
+    }
+}
+
+
+// MARK: - GiphyRequestable
+fileprivate protocol GiphyRequestable {
+    var urlComponents: String { get }
+    var url: URL { get }
+}
+
+extension GiphyRequestable {
+    var url: URL {
+        let url = Giphy.baseUrl + urlComponents + "&api_key=\(Giphy.apiKey)"
+        return URL(string: url)!
     }
 }
