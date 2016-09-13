1b75e91cdd0df7fbec08332c632ca21d6f67382d
Matias Seijas <matiseijas@gmail.com>
2016-09-13 08:10:57 -0400

    Init playground

--- GiphySwift.playground/Contents.swift
+++ GiphySwift.playground/Contents.swift
@@ -1,5 +1,64 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
+import PlaygroundSupport
 
-var str = "Hello, playground"
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+struct Giphy {
+    
+    static let host = "api.giphy.com"
+    static let apiVersion = "v1"
+    static let baseUrl = "https://\(host)/\(apiVersion)"
+    
+    enum Endpoint {
+        case search(phrase: String)
+        case gif(id: String)
+        case gifs(ids: [String])
+        case translate(phrase: String)
+        
+        var url: URL {
+            var url = Giphy.baseUrl
+            
+            switch self {
+            case .search(let phrase): url += "/gifs/search?q=\(phrase)"
+            default: break
+            }
+            
+            url += "&api_key=\(Giphy.apiKey)"
+            
+            return URL(string: url)!
+        }
+    }
+    
+    enum Rating {
+        case y, g, pg, pg13, r
+    }
+    
+    private(set) static var apiKey = "dc6zaTOxFJmzC"
+    
+    func configure(apiKey: String) {
+        Giphy.apiKey = apiKey
+    }
+    
+    static func request(_ endpoint: Endpoint) {
+        let urlRequest = URLRequest(url: endpoint.url)
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            print("data: \(data)")
+            print("response: \(response)")
+            print("error: \(error)")
+            
+            if let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) {
+                
+                print(json)
+                
+            }
+            }.resume()
+    }
+    
+}
+
+//Giphy.request(.search(phrase: "britney"))
+
