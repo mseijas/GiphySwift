e27ed4c6f8d9e284dafe9433dcc2a5cd1828785d
Matias Seijas <matiseijas@gmail.com>
2016-10-01 17:14:41 -0400

    Configure CollectionViewController for remaining request types

--- GiphySwift-Example/GiphySwift-Example/CollectionViewController.swift
+++ GiphySwift-Example/GiphySwift-Example/CollectionViewController.swift
@@ -13,6 +13,10 @@ import FLAnimatedImage
 class ImageCell: UICollectionViewCell {
     static let reuseIdentifier = "ImageCell"
     @IBOutlet weak var imageView: FLAnimatedImageView!
+    
+    override func prepareForReuse() {
+        imageView.image = nil
+    }
 }
 
 class CollectionViewController: UICollectionViewController {
@@ -20,32 +24,106 @@ class CollectionViewController: UICollectionViewController {
     var images = [GiphyImage]()
     var config: (section: Section, row: Row)!
     
+    let layout = UICollectionViewFlowLayout()
+    let searchBar = UISearchBar()
+    var displaySearchBar = false
+    
     private let queue = DispatchQueue(label: "com.giphyswift.example", qos: .userInteractive, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        requestImages(with: config)
+
+        requestImages(with: config, searchText: nil)
         title = "\(config.section.title): \(config.row.title)"
+        
+        layout.headerReferenceSize = CGSize(width: 100, height: displaySearchBar == true ? 44 : 0)
+        layout.itemSize = CGSize(width: 120, height: 120)
+        collectionView?.collectionViewLayout = layout
+        
+        searchBar.delegate = self
     }
     
-    func requestImages(with config: (section: Section, row: Row)?) {
+    func requestImages(with config: (section: Section, row: Row)?, searchText: String?) {
         guard let config = config else { return }
 
         switch config.section {
         case .gifs:
             switch config.row {
             case .trending:
-                Giphy.Gif.request(.trending) { result in
-                    if case .success(let images) = result {
-                        self.images = images.result
-                        self.updateUI()
-                    }
+                Giphy.Gif.request(.trending, completionHandler: processResult)
+            
+            case .search:
+                guard let searchText = searchText else {
+                    displaySearchBar = true
+                    return
                 }
                 
-            default: break
+                Giphy.Gif.request(.search(searchText), completionHandler: processResult)
+                
+            case .translate:
+                guard let searchText = searchText else {
+                    displaySearchBar = true
+                    return
+                }
+                
+                Giphy.Gif.request(.translate(searchText), completionHandler: processResult)
+                
+            case .byID:
+                guard let searchText = searchText else {
+                    displaySearchBar = true
+                    return
+                }
+                
+                Giphy.Gif.request(.withId(searchText), completionHandler: processResult)
+                
+            case .random:
+                displaySearchBar = true
+                Giphy.Gif.request(.random(tag: searchText), completionHandler: processResult)
             }
             
-        default: break
+        case .stickers:
+            switch config.row {
+            case .trending:
+                Giphy.Sticker.request(.trending, completionHandler: processResult)
+                
+            case .search:
+                guard let searchText = searchText else {
+                    displaySearchBar = true
+                    return
+                }
+                
+                Giphy.Sticker.request(.search(searchText), completionHandler: processResult)
+                
+            case .translate:
+                guard let searchText = searchText else {
+                    displaySearchBar = true
+                    return
+                }
+                
+                Giphy.Sticker.request(.translate(searchText), completionHandler: processResult)
+                
+            case .random:
+                displaySearchBar = true
+                Giphy.Sticker.request(.random(tag: searchText), completionHandler: processResult)
+                
+            default: return
+            }
+        }
+    }
+    
+    func processResult(result: Any) {
+        if let result = result as? GiphyRequestResult {
+            if case .success(let images) = result {
+                self.images = images.result
+                self.updateUI()
+            }
+        }
+        
+        if let result = result as? GiphyRandomRequestResult {
+            if case .success(let images) = result {
+                self.images = images.result
+                self.updateUI()
+            }
         }
     }
     
@@ -92,11 +170,28 @@ class CollectionViewController: UICollectionViewController {
             return URL(string: image.images.fixedHeight.downsampled.gif.url)
         }
         
+        // NOTE: The Giphy API returns these URLs as HTTP - Here we convert them first to HTTPS
         if let image = image as? GiphyRandomImageResult {
-            return URL(string: image.images.fixedHeight.downsampled.gif.url)
+            var urlComponents = URLComponents(string: image.images.fixedHeight.downsampled.gif.url)
+            urlComponents?.scheme = "https"
+            return urlComponents?.url
         }
         
         return nil
     }
     
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionElementKindSectionHeader else { return UICollectionReusableView() }
+        let reusableView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "Header", for: indexPath)
+        reusableView.addSubview(searchBar)
+        searchBar.sizeToFit()
+        return reusableView
+    }
+    
+}
+
+extension CollectionViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        requestImages(with: config, searchText: searchBar.text)
+    }
 }
